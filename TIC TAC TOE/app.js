8e3705bbd61733af4db9c427723bbd72959ae171
Anshul2202 <anshul220206@gmail.com>
2025-07-15 12:18:26 +0530

    Making some new changes

--- TIC TAC TOE/app.js
+++ TIC TAC TOE/app.js
@@ -1,84 +1,78 @@
 let boxes = document.querySelectorAll(".box");
+let display = document.querySelector(".display");
+let msg = document.querySelector("#msg");
+let newBtn = document.querySelector("#newBtn");
+let reset = document.querySelector("#reset");
 
+let turn = true;
 
-let abc = true;
-
-
-boxes.forEach((i) => {
-
-   let task = () => {
-      if(abc == true){
-         i.innerText ="X";
-         abc = false;
-         i.disabled = true;
-         checkWinner();
-      }else if(abc == false){
-         i.innerText ="O";
-         abc = true;
-         i.disabled = true;
-         checkWinner();
-      }
-   }
-   
-   i.addEventListener("click" , task);
-
-
-     
-})
-
-let chances = [ [0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,8] , [2,4,6]];
-
-let bigBox =document.querySelector(".bigBox");
-
-let checkWinner = () => {
-   
-   for (let i of chances){
-
-     if(boxes[i[0]].innerText =="X" && boxes[i[1]].innerText =="X" && boxes[i[2]].innerText == "X"){
-
-
-      bigBox.innerText = "Winner Is X [Reload This Page To Play Again]";
-      bigBox.style.backgroundColor = "ffafcc";
-      bigBox.style.fontSize = "50px";
-      bigBox.style.border =" 2px solid black";
-      bigBox.style.borderRadius = "10px";
-      bigBox.style.width = "1200px";
-      bigBox.style.position = "relative";
-      bigBox.style.left = "120px";
-      bigBox.style.color = "white";
-
-
-     }else if(boxes[i[0]].innerText =="O"&& boxes[i[1]].innerText =="O" && boxes[i[2]].innerText == "O"){
-
-
-      bigBox.innerText = "Winner Is O [Reload This Page To Play Again]";
-      bigBox.style.backgroundColor = "ffafcc";
-      bigBox.style.fontSize = "50px";
-      bigBox.style.border =" 2px solid black";
-      bigBox.style.borderRadius = "10px";
-      bigBox.style.width = "1200px";
-      bigBox.style.position = "relative";
-      bigBox.style.left = "120px";
-      bigBox.style.color = "white";
-     }
-
+for(let i of boxes){
+    i.addEventListener("click" , () => {
+        if(turn === true){
+            i.innerText = "X";
+            turn = false;
+            i.disabled = true;
+            checkWinner();
+        } else if(turn === false){
+            i.innerText = "O";
+            turn = true;
+            i.disabled = true;
+            checkWinner();
+        }
+    })
+    
 }
 
+function vanish(){
+    for(let i of boxes){
+        i.innerText = "";
+        i.disabled = false;
 
+    }
+}
+
+function NewGame(){
+    turn = true;
+    vanish();
+    display.classList.add("hide");
+}
+
+function block(){
+    for(let i of boxes){
+        i.disabled = true;
+    }
+}
+
+function displayWinner(winner) {
+    msg.innerText = `Winner is ${winner}`;
+    display.classList.remove("hide");
 }
 
 
 
-let reset = document.querySelector("#reset-btn");
+let winPattern = [[0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,8] , [2,4,6]];
 
-let set = () => {
-   boxes.forEach((i) => {
-      i.innerText = "";
-      i.disabled = false;
-   })
- 
- 
+function checkWinner (){
+    
+    for(let i of winPattern){
+        let val1 = boxes[i[0]].innerText;
+        let val2 = boxes[i[1]].innerText;
+        let val3 = boxes[i[2]].innerText;
+
+        if(val1 === "X" && val2 ==="X" && val3 === "X"){
+            console.log("winner is X");
+            displayWinner("X");
+            block();
+        } else if(val1 === "O" && val2 ==="O" && val3 === "O"){
+            console.log("winner is O");
+            displayWinner("O");
+            block();
+        }
+
+    }
+
 }
 
+newBtn.addEventListener("click" , NewGame);
 
-reset.addEventListener("click" , set);
+reset.addEventListener("click" , NewGame);
